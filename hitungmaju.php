7e32405bad54011655423828ff7fb7a0dcc9704e
Muhammad Al Washil Al Majidi <washilmajidi1999@gmail.com>
2022-01-17 09:49:09 +0700

    syntax while

---
+++ hitungmaju.php
@@ -0,0 +1,12 @@
+<?php
+echo "HITUNG MAJU ";
+
+$a = 1;
+while ($a <= 20){
+    if ($a %2 ==0){
+        echo "$a ";
+    }
+   
+    $a++;
+}
+?>
---
+++ hitungmundur.php
@@ -0,0 +1,12 @@
+<?php
+echo "HITUNG MUNDUR ";
+
+$a = 20;
+while ($a >= 1){
+    if ($a %2 ==0){
+        echo "$a ";
+    }
+   
+    $a--;
+}
+?>
